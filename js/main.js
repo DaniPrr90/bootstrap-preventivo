c95536b0ef66bb8f9cb03f7d82dac6c969d22923
DaniPrr90 <danilo.pirro@libero.it>
2024-11-08 19:35:12 +0100

    add: iserimento ciclo fo

--- js/main.js
+++ js/main.js
@@ -1,42 +1,86 @@
-console.log("sono nella console")
-
-
-
-
 /*---------Variabili di html---------*/
-const myForm = document.getElementById ("myform");
-const name = document.getElementById ("Name");
-const firstname = document.getElementById ("firstName");
-const email = document.getElementById ("insertEmail");
-const jobType = document.getElementById ("job");
-const textArea = document.getElementById ("Textarea");
-const codePromo = document.getElementById ("code");
-const privacy = document.getElementById ("privacyPolicy");
+const formPreventivo = document.getElementById("formPreventivo");
 
-
-/* ------------logica delle variabili------------ */
-
-
-
-
-
-
-
-
+/**************************** */
 
 /*--------Definisco una variabile con i servizi per la lezione---------*/
 
-const lessonServices = [
-    { name: 'Backend Development', servicePrice: 20.50 },
-    { name: 'Frontend Development', servicePrice: 15.30 },
-    { name: 'Project Analysis', servicePrice: 33.60 }
-];
+const backendDevelopment = 20.50;
+const frontendDevelopment = 15.30;
+const projectAnalysis = 33.60
+
+/* Definisco i codici sconto e il valore dello sconto da applicare */
+const discountPrice = 25;
+const discountCodes = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
+
+/* Definisco il numero di ore totali per un lavoro */
+
+const hourJobs = 10;
+
+/* ------------inserimento condizionali------------ 
+
+● se la commissione riguarda lo sviluppo backend il prezzo orario è di 20.50€/l’ora
+*/
+
+formPreventivo.addEventListener("submit", function (event)){
+
+    event.preventDefault();
+
+    //Leggo i valori dal HTML
+    const jobType = document.getElementById("jobType");
+    const areaMessage = document.getElementById("areaMessage");
+    const codePromo = document.getElementById("codePromo");
+    const privacyPolicy = document.getElementById("privacyPolicy");
+    const name = document.getElementById("name");
+    const firstName = document.getElementById("firstName");
+    const mail = document.getElementById("mail");
+
+
+    // Determino la tipologia di jobType
+    // Calcolo il totale in base al servicePrice * totale ore (sempre 10)
+    let totalPrice;
+    if (jobType == 1 ){
+        totalPrice = backendDevelopment * 10
+        console.log(totalPrice);
+    }
+    if(jobType == 2){
+        totalPrice = frontendDevelopment * 10
+    }
+    if(jobType == 3){
+        totalPrice = projectAnalysis * 10
+    }
+
+// codePromo = 'SCONTO'
+if (codePromo == "YHDNU32"){
+
+}
+/* verifico se il codice sconto esiste nell'array e lo applico allo sconto */
+
+for(let i = 0; i < discountCodes.length; i++ ){
+totalPrice = backendDevelopment - (totalPrice * (discountPrice) / 100);
+totalPrice = frontendDevelopment - (totalPrice * (discountPrice) / 100);
+totalPrice = projectAnalysis -(totalPrice * (discountPrice) / 100);
+
+}
+    // Controllo se è il codice sia valido e applico lo sconto
+    // Se il codice non è valido mostro un messaggio all'utente
+
+
+    
+
+}
+
+
+
+
+
 
 /*----------variabili di sconto-------------*/
 
-const discountPrice = 25;
-const discountCodes = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
-const discountCodesLength = discountCodes.length;
+
+
+
+
 
 
 
